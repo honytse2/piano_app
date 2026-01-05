b70a7696227458354990a52af208a36c8cca8dbd
honytse2 <honytse2@gmail.com>
2026-01-05 23:33:18 +0800

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
+const CACHE_NAME = 'piano-v1';
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
-
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
